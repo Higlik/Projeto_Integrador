d82ef59306abb6a2801fbc0e79c5714fddd29297
Giulia <giugalbi@hotmail.com>
2021-12-10 11:26:35 -0300

    Task6

---
+++ back-end/ecommerce/src/main/java/br/com/integrador/projeto/ecommerce/model/Usuario.java
@@ -0,0 +1,84 @@
+package br.com.integrador.projeto.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome deve ser preenchido.")
+	private String nome;
+	
+	@NotBlank(message = "O usuário deve ser preenchido.")
+	@Email
+	private String usuario;
+	
+	@NotBlank(message = "A senha deve ser preenchida.")
+	@Size(min = 8, max = 8, message = "A senha deve conter 8 caracteres.")
+	private String senha;
+	
+	@NotBlank(message = "O telefone deve ser preenchido.")
+	@Size(min = 11, max = 11, message = "O telefone deve conter o DDD + os 9 números.")
+	private int telefone;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+}
