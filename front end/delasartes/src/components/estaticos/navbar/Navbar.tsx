dcdb393f2b9cc4c9aeba1bd94bbf875782a24810
Victor Silva de Franca <victor.silva.franca18@gmail.com>
2022-01-21 15:09:39 -0300

    subindo navbar

--- front end/delasartes/src/components/estaticos/navbar/Navbar.tsx
+++ front end/delasartes/src/components/estaticos/navbar/Navbar.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import './navbar.css';
-const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
+import React from "react";
+import { alpha, Box, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <div>
+                <div  className="displayNavBarLogo">
+                   
+                   <Link to='./home' className="text-decoration-none">
+                    <Box className="cursor"  >
+                        <Typography variant="h5" color="inherit" className="logoDA" >
+                            Delas Artes
+                        </Typography>
+                     </Box>
+                    </Link>
+
+                    <Box className="displayNavbar">
+                    <Link to='./sobre' className="text-decoration-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit" className="FontNavbar">
+                                Sobre
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                        <Link to='./contato' className="text-decocartor-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit" className="FontNavbar">
+                                    Contato
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to='./produtos' className="text-decocartor-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit" className="FontNavbar">
+                                    Produtos
+                                </Typography>
+                            </Box>
+                        </Link>
+
+
+                        <Link to='/login' className="text-decocartor-none">
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit" className="FontNavbar">
+                                    Categorias
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className="cursor" >
+                                <Typography variant="h6" color="inherit" className="LgLoginNavBar">
+                                </Typography>
+                            </Box>
+                        </Link>
+
+
+                    </Box>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Navbar
+
+
+
+export default Navbar;
