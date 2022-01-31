62011d6897943d9474a5e3ee2ec2bbf545e0a788
Pedro Henrique Jones <pedrohenriquejones35@gmail.com>
2022-01-31 17:26:15 -0300

    Estilizando a Navbar e a Home Page

--- front end/delasartes/src/components/estaticos/navbar/Navbar.tsx
+++ front end/delasartes/src/components/estaticos/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import SearchIcon from '@mui/icons-material/Search';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import PersonIcon from '@mui/icons-material/Person';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { AppBar, Toolbar, alpha, Box, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
@@ -10,81 +12,90 @@ import './navbar.css';
 function Navbar() {
     return (
         <>
-            <div>
-                <div  className="displayNavBarLogo backgroundNavBar">
-                   
-                   <Link to='./home' className="text-decoration-none">
-                    <Box className="logoDA"  >
+
+            <div className="displayNavBar backgroundNavBar">
+
+                <Link to='./home' className="text-decoration-none">
+                    <Box  >
                         <Typography variant="h5">
-                            delasArtes
+                            <h1 className="logoDA" >delasArtes</h1>
                         </Typography>
-                     </Box>
-                    </Link>
+                    </Box>
+                </Link>
 
-
-                    <Link to='/produto' className='text-decorator-none'>
-                            <Box mx={1} className="cursor" >
-                                <Typography variant="h6" color="inherit" className="LgLoginNavBar">
-                                   Produtos
-                                </Typography>
-                            </Box>
-                        </Link>
-
-
-                    <Box className="displayNavbar">
-                    <Link to='./categorias' className="text-decoration-none">
+                <Box className="font displayitens">
+                    <Link to='./sobre' className="text-decocartor-none">
                         <Box mx={1} className="cursor">
-                            <Typography variant="h6" color="inherit" className="FontNavbar">
-                                categorias
+                            <Typography variant="h6" className="FontNavbar">
+                                <p >Sobre</p>
                             </Typography>
                         </Box>
                     </Link>
 
-                        <Link to='./artistas' className="text-decocartor-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="inherit" className="FontNavbar">
-                                    artistas
-                                </Typography>
-                            </Box>
-                        </Link>
+                    <Link to='/usuario' className="text-decocartor-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" className="FontNavbar">
+                                <p>Contato</p>
+                            </Typography>
+                        </Box>
+                    </Link>
 
-                        <Link to='./sobre' className="text-decocartor-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="inherit" className="FontNavbar">
-                                    sobre
-                                </Typography>
-                            </Box>
-                        </Link>
-
-
-                        <Link to='' className="text-decocartor-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="inherit" className="FontNavbar">
-                                    
-                                </Typography>
-                            </Box>
-                        </Link>
-                        
-                        <Link to='/usuario' className="text-decocartor-none">
-                            <Box mx={1} className="cursor">
-                                <Typography variant="h6" color="inherit" className="FontNavbar">
-                                    Meu Perfil
-                                </Typography>
-                            </Box>
-                        </Link>
-
+                    <Link to='/produto' className='text-decorator-none'>
+                        <Box mx={1} className="cursor" >
+                            <Typography variant="h6" className="LgLoginNavBar">
+                                <Box className="displayitens">
+                                <p >Produtos</p>
+                                <KeyboardArrowDownIcon />
+                                </Box>
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to='./categorias' className="text-decoration-none">
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" className="FontNavbar">
+                            <Box className="displayitens">
+                                <p>Categorias</p>
+                                <KeyboardArrowDownIcon />
+                                </Box>
+                            </Typography>
+                        </Box>
+                    </Link>
+                    </Box>
+                    
+                    <Box className="displayicones">
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className="cursor" >
-                                <Typography variant="h6" color="inherit" className="LgLoginNavBar">
-                                    Login
+                                <Typography variant="h6" className="LgLoginNavBar">
+                                    <AddBoxIcon className="tamanhoicones"/>
                                 </Typography>
                             </Box>
                         </Link>
-
-
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className="cursor" >
+                                <Typography variant="h6" className="LgLoginNavBar">
+                                    <SearchIcon className="tamanhoicones" />
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className="cursor" >
+                                <Typography variant="h6" className="LgLoginNavBar">
+                                   <LocalMallIcon className="tamanhoicones" />
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className="cursor" >
+                                <Typography variant="h6" className="LgLoginNavBar">
+                                    <PersonIcon className="tamanhoicones" />
+                                </Typography>
+                            </Box>
+                        </Link>
                     </Box>
 
-                </div>
+
+
+               
             </div>
         </>
     )
--- front end/delasartes/src/paginas/home/Home.tsx
+++ front end/delasartes/src/paginas/home/Home.tsx
@@ -3,8 +3,10 @@ import './home.css';
 import TelaProduto from '../../components/estaticos/produtos/telaProduto/TelaProduto';
 const Home = () => {
   return <div>
-    <img className='imgInicial' src="https://i.imgur.com/pZQAIBh.jpg" alt="Imagem de Ceramicas" />
-    <h1  className='fontTitulo mg_titulo'>Nossos Produtos</h1>
+    <img className='imgInicial' src="https://imgur.com/R1kPPuc.jpg" alt="Imagem de Ceramicas" />
+    <h1  className='fontTitulo mg_titulo'>ARTE + ARTESANATO + ESCULTURAS E MUITO MAIS!</h1>
+    <div className='border'></div>
+    <h3 className='titulos'>ARTISTAS QUE VOCÊ PRECISA CONHECER</h3>
     <TelaProduto/>
   </div>;
   
