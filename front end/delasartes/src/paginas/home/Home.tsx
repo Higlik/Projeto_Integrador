e0bf944f5c1234aa2af64b2b2bdc06c72f0a69b8
Pedro Henrique Jones <pedrohenriquejones35@gmail.com>
2022-02-03 17:04:16 -0300

    Agora sim vai

--- front end/delasartes/src/paginas/home/Home.tsx
+++ front end/delasartes/src/paginas/home/Home.tsx
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css';
 import Navbar from '../../components/estaticos/navbar/Navbar'
 import CaroucelComponent from '../../components/caroucelComponent/CaroucelComponent';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+
+}, [])
   return <div>
    
     <CaroucelComponent />
-    <Navbar />
+   
     <h1 className='fontTitulo mg_titulo'>ARTE + ARTESANATO + ESCULTURAS + MUITO MAIS!</h1>
     <div className='border'></div>
     <h3 className='titulos '>PINTURAS</h3>
 
     <div className='imgartesanato'>
       <Link to='/produto/1'>
-        <img src="https://imgur.com/uWnupVN.png" alt="" />
+        <img className='zoom'src="https://imgur.com/uWnupVN.png" alt="" />
       </Link>
       <Link to='/produto/2'>
-        <img src="https://imgur.com/72VcnPy.png" alt="" />
+        <img className='zoom' src="https://imgur.com/72VcnPy.png" alt="" />
       </Link>
       <Link to='/produto/3'>
-        <img src="https://imgur.com/2XbgVHh.png" alt="" />
+        <img className='zoom' src="https://imgur.com/2XbgVHh.png" alt="" />
       </Link>
       <Link to='/produto/4'>
-        <img src="https://imgur.com/qaw3Wsl.png" alt="" />
+        <img className='zoom' src="https://imgur.com/qaw3Wsl.png" alt="" />
       </Link>
     </div>
-
+    
     <h3 className='titulos'>ARTESANATO</h3>
     <div className='imgartesanato'>
-      <Link to='/produto/5'>
+      <Link className='zoom' to='/produto/5'>
         <img src="https://imgur.com/C6c4TPl.png" alt="" />
       </Link>
-      <Link to='/produto/6'>
+      <Link className='zoom' to='/produto/6'>
         <img src="https://imgur.com/lEze8cS.png" alt="" />
       </Link>
-      <Link to='/produto/8'>
+      <Link className='zoom' to='/produto/8'>
         <img src="https://imgur.com/4xcJnKI.png" alt="" />
       </Link>
-      <Link to='/produto/9'>
+      <Link className='zoom' to='/produto/9'>
         <img src="https://imgur.com/SYQxj9n.png" alt="" />
       </Link>
 
@@ -47,19 +51,19 @@ const Home = () => {
     <h3 className='titulos'>ESCULTURAS</h3>
     <div className='imgartesanato'>
       <Link to='/produto/10'>
-        <img src="https://imgur.com/2TaG4ad.png" alt="" />
+        <img className='zoom' src="https://imgur.com/2TaG4ad.png" alt="" />
       </Link>
-      <Link to='/produto/11'>
+      <Link className='zoom' to='/produto/11'>
         <img src="https://imgur.com/ETPhkAA.png" alt="" />
       </Link>
-      <Link to='/produto/12'>
+      <Link className='zoom' to='/produto/12'>
         <img src="https://imgur.com/Fq0YOjc.png" alt="" />
       </Link>
-      <Link to='/produto/13'>
+      <Link className='zoom' to='/produto/13'>
         <img src="https://imgur.com/qogVRGN.png" alt="" />
       </Link>
     </div>
-
+    <Navbar />
   </div>;
 
 };
