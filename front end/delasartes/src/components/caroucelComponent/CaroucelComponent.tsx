ce1464b634291d9d0732e40594c4c409b8b7981c
Pedro Henrique Jones <pedrohenriquejones35@gmail.com>
2022-02-04 08:43:21 -0300

    adicionando mais intervalo no carousel

--- front end/delasartes/src/components/caroucelComponent/CaroucelComponent.tsx
+++ front end/delasartes/src/components/caroucelComponent/CaroucelComponent.tsx
@@ -16,98 +16,98 @@ function CarouselComponent() {
       alt="First slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel2"
       src="https://imgur.com/7Z3jzqu.png"
       alt="Second slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/TL1SMxr.png"
       alt="Third slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/TZFCLRd.png"
       alt="Fourth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/fnibwVl.png"
       alt="Fifth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/zCQUyOF.png"
       alt="Sixth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/akutqzb.png"
       alt="Seventh slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/F5O08Tj.png"
       alt="Eighth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/AQik5vB.png"
       alt="Ninth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/08xTGJU.png"
       alt="Tenth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/eQ1Tc2p.png"
       alt="Eleventh slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/64uEMPt.png"
       alt="Twelfth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/dHLWfQf.png"
       alt="Thirteenth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/HV3oqMM.png"
       alt="Fourteenth slide"
     />
   </Carousel.Item>
-  <Carousel.Item interval={1000}>
+  <Carousel.Item interval={2000}>
     <img
       className="imgcarousel3"
       src="https://imgur.com/mcPJzki.png"
